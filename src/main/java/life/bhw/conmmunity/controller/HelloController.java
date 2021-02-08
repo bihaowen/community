b62ac270f5089f2cbd9f732a0a261fbf54a1a89b
毕浩文 <1150272783@qq.com>
2021-02-08 17:17:56 +0800

    第一次提交代码到gitHub

---
+++ src/main/java/life/bhw/conmmunity/controller/HelloController.java
@@ -0,0 +1,17 @@
+package life.bhw.conmmunity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHello(Model model, @RequestParam(name = "name") String name){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
