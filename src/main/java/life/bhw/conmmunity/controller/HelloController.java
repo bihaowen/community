fd98faf584a593c45456b5ed3070a713ae8e5ad0
毕浩文 <1150272783@qq.com>
2021-02-19 15:35:46 +0800

    登录注册

--- src/main/java/life/bhw/conmmunity/controller/HelloController.java
+++ src/main/java/life/bhw/conmmunity/controller/HelloController.java
@@ -3,7 +3,6 @@ package life.bhw.conmmunity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,6 +11,6 @@ public class HelloController {
     @GetMapping("/hello")
     public String getHello(Model model, @RequestParam(name = "name") String name){
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
     }
 }
